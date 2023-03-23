24d33ffb01b944c94faa6abc86f7c8334843132d
Elliot Voris <elliot@voris.me>
2023-03-23 16:42:05 -0500

    minor tweaks to asset overview
    Signed-off-by: Elliot Voris <elliot@voris.me>

--- src/routes/dashboard/AssetStats.svelte
+++ src/routes/dashboard/AssetStats.svelte
@@ -3,16 +3,25 @@
 </div>
 <div class="stats bg-primary text-primary-content w-full">
     <div class="stat">
-        <div class="stat-title">Lumens balance</div>
-        <div class="stat-value">89,400 XLM</div>
+        <div class="stat-title">Lumens Balance</div>
+        <div class="stat-value">9,467.<sup>7519327</sup> XLM</div>
         <div class="stat-actions">
             <button class="btn btn-sm btn-success">Add funds</button>
         </div>
     </div>
 
     <div class="stat">
-        <div class="stat-title">USDC balance</div>
+        <div class="stat-title">USDC Balance</div>
         <div class="stat-value">$250.<sup>13</sup></div>
+        <div class="stat-actions">
+            <button class="btn btn-sm">Withdrawal</button>
+            <button class="btn btn-sm">deposit</button>
+        </div>
+    </div>
+
+    <div class="stat">
+        <div class="stat-title">BTC Balance</div>
+        <div class="stat-value">2.<sup>0258937</sup> BTC</div>
         <div class="stat-actions">
             <button class="btn btn-sm">Withdrawal</button>
             <button class="btn btn-sm">deposit</button>

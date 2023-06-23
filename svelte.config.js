47731b1aacae2fda0fce40b1034b363f83acf585
Elliot Voris <elliot@stellar.org>
2023-06-23 18:46:17 -0500

    let's try with `index.html`

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ import adapter from '@sveltejs/adapter-static'
 const config = {
     kit: {
         adapter: adapter({
-            fallback: '200.html',
+            fallback: 'index.html',
         }),
     },
     prerender: false,

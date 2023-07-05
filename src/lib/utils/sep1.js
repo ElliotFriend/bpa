41cb2aa899c7f3c6c4ce403caeaf28808deea0f8
Elliot Voris <elliot@stellar.org>
2023-07-05 12:16:53 -0500

    Updating sep1
    - ordering things according to the sep1 document
    - including a couple get() functions for networkPassphrase, and
      federationServer
    
    Signed-off-by: Elliot Voris <elliot@stellar.org>

--- src/lib/utils/sep1.js
+++ src/lib/utils/sep1.js
@@ -1,30 +1,40 @@
 import { StellarTomlResolver } from 'stellar-sdk'
 
-export async function fetchStellarToml(homeDomain) {
-    return StellarTomlResolver.resolve(homeDomain)
+export async function fetchStellarToml(domain) {
+    return StellarTomlResolver.resolve(domain)
 }
 
-export async function getWebAuthEndpoint(homeDomain) {
-    let { WEB_AUTH_ENDPOINT } = await fetchStellarToml(homeDomain)
-    return WEB_AUTH_ENDPOINT
+export async function getNetworkPassphrase(domain) {
+    let { NETWORK_PASSPHRASE } = await fetchStellarToml(domain)
+    return NETWORK_PASSPHRASE
 }
 
-export async function getKycServer(homeDomain) {
-    let { KYC_SERVER, TRANSFER_SERVER } = await fetchStellarToml(homeDomain)
-    return KYC_SERVER ?? TRANSFER_SERVER
+export async function getFederationServer(domain) {
+    let { FEDERATION_SERVER } = await fetchStellarToml(domain)
+    return FEDERATION_SERVER
 }
-
-export async function getServerSigningKey(homeDomain) {
-    let { SIGNING_KEY } = await fetchStellarToml(homeDomain)
-    return SIGNING_KEY
-}
-
-export async function getTransferServerSep6(homeDomain) {
-    let { TRANSFER_SERVER } = await fetchStellarToml(homeDomain)
+export async function getTransferServerSep6(domain) {
+    let { TRANSFER_SERVER } = await fetchStellarToml(domain)
     return TRANSFER_SERVER
 }
 
-export async function getTransferServerSep24(homeDomain) {
-    let { TRANSFER_SERVER_SEP0024 } = await fetchStellarToml(homeDomain)
+export async function getTransferServerSep24(domain) {
+    let { TRANSFER_SERVER_SEP0024 } = await fetchStellarToml(domain)
     return TRANSFER_SERVER_SEP0024
 }
+
+export async function getKycServer(domain) {
+    let { KYC_SERVER, TRANSFER_SERVER } = await fetchStellarToml(domain)
+    return KYC_SERVER ?? TRANSFER_SERVER
+}
+
+export async function getWebAuthEndpoint(domain) {
+    let { WEB_AUTH_ENDPOINT } = await fetchStellarToml(domain)
+    return WEB_AUTH_ENDPOINT
+}
+
+export async function getServerSigningKey(domain) {
+    let { SIGNING_KEY } = await fetchStellarToml(domain)
+    return SIGNING_KEY
+}
+
